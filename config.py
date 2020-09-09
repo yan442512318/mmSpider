a86865f83756b3abffc78dc615a4cb516ba608f1
yan442512318 <yan442512318@gmail.com>
2020-09-09 23:16:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ MONGO_DB = 'maimai'
 KEYWORD = "数据分析"
 
 HEADERS = {
-    'cookie': 'buuid=476a6162-25d2-4eae-a0f5-d903be018fb0; seid=s1599469473086; guid=GxsbBBgEGx4aBBsZVh0SEhwbHR9WHBkEHRkfBUNYS0xLeQoZEgQZEhseBBoEHxIFT0dFWEJpCgNFQUlPbQpPQUNGCgZmZ35iYQIKHBkEHRkfBV5DYUhPfU9GWlprCgMeHFIKER4cREN9ChEaBBobCn5kClldRU5EQ30CChoEHwVLRkZDUEVn; u=204278645; maimai_version=4.0.0; channel=www; session=eyJzZWNyZXQiOiJsYUdSU0VsUG9RSW1uUDFvVjBmaURTakYiLCJ1IjoiMjA0Mjc4NjQ1IiwiX2V4cGlyZSI6MTU5OTYzNDAzNjUwOCwiX21heEFnZSI6ODY0MDAwMDB9; session.sig=X1kAM5WLUG-xOASjk-d3h8cKLfk; is_company_vip=0; has_admin=1; access_token=1.7e138c19c8fff0aed2040e6e53f4bcd7; biz:jobs:session=eyJ1IjoiMjA0Mjc4NjQ1IiwiYWNjZXNzX3Rva2VuIjoiMS43ZTEzOGMxOWM4ZmZmMGFlZDIwNDBlNmU1M2Y0YmNkNyIsInZlcnNpb24iOiI0LjAuMCIsIl9leHBpcmUiOjE1OTk2MzQwNzg0OTEsIl9tYXhBZ2UiOjg2NDAwMDAwfQ==; company_time=1599547788993; csrftoken=J6IsUozE-jxSEzZ-CcOl8bnI3QCvRRsJgOfI',
+    'cookie': '',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
     'sec-fetch-dest': 'empty',
